a6ef2cbabcb8d8f776e25046c56cff977bca3078
KacperOG <s26213@pjwstk.edu.pl>
2021-11-06 20:34:37 +0100

    ćwiczenie 2 dodawanie

---
+++ zadanie1cw2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int a=0;
+int b=0;
+auto main () -> int
+{
+std :: cout << "Podaj pierwsza liczbe !\n";
+std :: cin >> a;
+std :: cout << "Podaj druga liczbe !\n";
+std:: cin >> b;
+std :: cout << a+b;
+return 0;
+}

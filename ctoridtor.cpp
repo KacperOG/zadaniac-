520e31224896278614521873566857ff5b066e8c
KacperOG <93011012+KacperOG@users.noreply.github.com>
2022-01-28 13:37:49 +0100

    Update ctoridtor.cpp

--- ctoridtor.cpp
+++ ctoridtor.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
-#include <string>
 
+struct dstr //stuktura o nazwie dstr
+{
+    std::string ds1; //random pole1
+    std::string ds2; //random pole2
 
-
-struct Student{
-	private: 
-	std::string name;
-	std::string surname;
-	
-	public:	
-	Student(std::string n, std::string s){
-		name = n;
-		surname = s;
-	}
-	~Student(){
-		std::cout<<"DESTRUCTION!"<<std::endl;
-		std::cout<<name<<std::endl;
-		std::cout<<surname<<std::endl;
-	}
+    dstr(std::string a, std::string b){ //konstruktor i wkladamy do srodka zmienne (stwarza obiekt)
+        ds1 = a; //przypisanie zmiennych z konstruktora
+        ds2 = b;
+    }
+    ~dstr(){ //destruktor- niszczy strukta
+        std::cout << ds1 << "\n"; //przypisanie zmiennych z dekonstruktora
+        std::cout << ds2 << "\n";
+        std::cout << "DESTRUCTION!"; //wypisuje destruction
+    }
 };
 
 
-auto main() -> int
+auto main()-> int
 {
-	auto student = Student{"K", "G"};
-	
-	return 0;
+    auto ndstr = dstr("2", "1"); //nowa struktura przechodzi do dstr i potem do ~dstr
+    return 0;
 }
